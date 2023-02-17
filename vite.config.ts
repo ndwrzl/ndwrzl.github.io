f377a63327c4f4b9e62be56212bf1499db275dc2
Ander <ndwrzl@protonmail.com>
2023-02-17 12:56:44 +0100

    Update packages

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import mkcert from "vite-plugin-mkcert";
 import { resolve } from "node:path";
 
 export default defineConfig(() => {
@@ -14,14 +13,8 @@ export default defineConfig(() => {
         },
       ],
     },
-    server: {
-      https: true,
-    },
     plugins: [
       react(),
-      mkcert({
-        source: "coding",
-      }),
     ],
     build: {
       rollupOptions: {
